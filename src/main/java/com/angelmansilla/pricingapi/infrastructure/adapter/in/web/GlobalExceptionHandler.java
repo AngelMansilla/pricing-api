c259f0703db508706b41ce9bd63a224fb1902ffd
Ángel Mansilla Puerto <angelmansillapuerto@outlook.com>
2026-04-26 12:33:37 +0200

    feat: expose price lookup endpoint

---
+++ src/main/java/com/angelmansilla/pricingapi/infrastructure/adapter/in/web/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.angelmansilla.pricingapi.infrastructure.adapter.in.web;
+
+import com.angelmansilla.pricingapi.domain.exception.PriceNotFoundException;
+import com.angelmansilla.pricingapi.infrastructure.adapter.in.web.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(PriceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlePriceNotFound(PriceNotFoundException exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+}
